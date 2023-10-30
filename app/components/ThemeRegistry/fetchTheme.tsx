cd9a44179c007652c98924b404f3cdefa9934d0f
cck37 <ckennedy0323@gmail.com>
2023-10-30 10:51:25 -0400

    fix: no need to fetch theme via a route

--- app/components/ThemeRegistry/fetchTheme.tsx
+++
@@ -1,7 +0,0 @@
-import { CategoryResponse } from "@/app/types";
-
-export async function fetchTheme() {
-  const res = await fetch("/api/category");
-  const { theme: themeOptions }: CategoryResponse = await res.json();
-  return themeOptions;
-}
