7b3a5e87bea6e4f02a17c03f788503570858df7b
cck37 <ckennedy0323@gmail.com>
2023-11-13 12:34:04 -0500

    fix: revalidation is broken so force dynamic it is

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,10 @@ import App from "./App";
 import Loading from "./loading";
 import { getCategory } from "./api/category/getCategory";
 
-export const revalidate = 3600; // revalidate the data at most every hour
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  const categoryInfo = getCategory(new Date().getSeconds());
+  const categoryInfo = getCategory(new Date().getDate());
   return (
     <Suspense fallback={<Loading />}>
       <App {...categoryInfo} />
