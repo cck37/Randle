c584607c19aff53a3967ced1d4f3a705da8524f5
cck37 <ckennedy0323@gmail.com>
2023-11-13 12:14:04 -0500

    test: now forcing revalidation b/c next sux

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,10 @@ import Loading from "./loading";
 import { getCategory } from "./api/category/getCategory";
 
 export const revalidate = 3600; // revalidate the data at most every hour
+export const dynamic = "force-dynamic";
 
-export default async function Home() {
-  const categoryInfo = getCategory(new Date().getDate());
+export default function Home() {
+  const categoryInfo = getCategory(new Date().getSeconds());
   return (
     <Suspense fallback={<Loading />}>
       <App {...categoryInfo} />
