a627a0e7150e6f4e8f5e1a4a3f0f720dd99d7326
Chris Kennedy <ckennedy0323@gmail.com>
2024-02-06 20:04:33 -0500

    fix: oops. also add error handling

--- app/api/guess/route.ts
+++ app/api/guess/route.ts
@@ -3,9 +3,30 @@ import { getGuessResponse } from "./getGuessResponse";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
-  const guess = searchParams.get("guess") ?? "";
-  const date = new Date(searchParams.get("date") ?? new Date().toString());
-  const data = await getGuessResponse(guess, date);
+  const guess = searchParams.get("guess");
+  const dateString = searchParams.get("date");
+
+  if (guess && guess.length <= 0)
+    return NextResponse.json(
+      {
+        error: "No guess provided",
+      },
+      {
+        status: 400,
+      }
+    );
+  if (dateString && dateString.length <= 0)
+    return NextResponse.json(
+      {
+        error: "No date provided",
+      },
+      {
+        status: 400,
+      }
+    );
+
+  const date = new Date(dateString ?? new Date());
+  const data = await getGuessResponse(guess ?? "", date);
 
   if (data) {
     // B/c ts claims Response.json(data) doesn't exist
